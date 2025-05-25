2ce703df95617115377ee93e334ca9be1fc3b950
abdurrahman2004 <ar978874@gmail.com>
2025-05-25 12:00:00 +0530

    Your commit message

--- src/main/java/com/abdur/BillingSoftware/BillingSoftwareApplication.java
+++ src/main/java/com/abdur/BillingSoftware/BillingSoftwareApplication.java
@@ -3,6 +3,13 @@ package com.abdur.BillingSoftware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Starter File for the Spring Boot Application
+//This file is the entry point for the Spring Boot application
+//It contains the main method which is the starting point of the application
+//It is annotated with @SpringBootApplication which enables auto-configuration, component scanning, and configuration properties
+//It is the main class that will be run when the application is started
+//It is the class that will be used to run the application
+
 @SpringBootApplication
 public class BillingSoftwareApplication {
 
